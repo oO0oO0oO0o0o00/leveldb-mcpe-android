80ac0f0461f9064f8cc2fd54b5b11f9ae9932128
oO0oO0oO0o0o00 <teabookshelf@keemail.me>
2018-02-25 10:46:10 +0800

    Add github files

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # leveldb-mcpe-android
 Ported Mojang's modified leveldb database to Android.
+Used to build Android apps in Android Studio to use leveldb
+database.
+It's intended to be included in apps to modify Minecraft
+game maps, so...
+The JNI part of this project has been hard-coded to use
+the configuration which is also used by Minecraft
+Bedrock Edition (May also be known as MinecraftPE).
+Such configuration can't be changed in Java code at least
+for now.
+
+## Based on
+* [@litl 's repo](https://github.com/litl/android-leveldb)
+    Android JNI version of Google's leveldb database, which most of
+    codes were copied from :)
+* [@Mojang 's modified leveldb](https://github.com/Mojang/leveldb-mcpe)
+* and [@Google 's original work](https://github.com/google/leveldb) of course:)
+
+## Usage
+
+Figure it out yourself...\
+Well, actually...I don't think anyone would use this thing...
+Here's some tips, if you really want more, open an issue to
+let me know..
+
+You can download an aar file 
+[here](https://github.com/oO0oO0oO0o0o00/leveldb-mcpe-android/releases)
+and include it in your Android Studio projects.\
+[How to use aar files?](https://www.google.com/search?&q=import+aar+android+studio&oq=import+aar+android+studio)\
+
+If you have ever used com.litl.leveldb you can just stick
+to the old ~~rabbit~~ habit:
+
+```java
+    //Open database
+    DB db = new DB(new File("/sdcard/iloverbq2012"));
+    db.open();
+    
+    //Put key/value pairs
+    db.put("test key".getBytes(),new byte[]{2,3,3,3});
+    
+    //Get value using key
+    byte[] result=db.get("test key".getBytes());
+    
+    //Close database
+    db.close();
+
+```
+
+For more you can just exam the demo's
+[code](https://github.com/oO0oO0oO0o0o00/leveldb-mcpe-android/blob/master/demo/src/main/java/rbq2012/leveldb/demo/ActivityMain.java)
+.
+
+You can also modify the code and compile by yourself.
+
+## How to build the project
+
+Open project in Android Studio, wait until it's finally
+ready (Maybe long enough for watching a Redtube video),
+click bunches of buttons to download and install various
+versions of things if you don't have SDK, Gradle and other
+things in exact same version as this project uses installed,
+during the time you could check up messages or whatever,
+depending on your net speed...
+
+Then you have the project opened now, plug your phone to
+computer if you don't have a virtual device created, click
+'Run' and wait another decade for Android Studio to compile
+java and C++ codes, generate an APK and have it installed on
+your phone or emulator...
+
+_(This section does nothing except complaining about how
+slow Android Studio is.)_
+
+## Original "Readme"s:
+* [@litl 's](https://github.com/litl/android-leveldb/blob/master/README.md)
+* [@mojang 's](https://github.com/Mojang/leveldb-mcpe/blob/master/README.md)
+
+### Meow meow meow~~~
--- demo/src/main/java/rbq2012/leveldb/demo/ActivityMain.java
+++ demo/src/main/java/rbq2012/leveldb/demo/ActivityMain.java
@@ -3,32 +3,38 @@ package rbq2012.leveldb.demo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import rbq2012.leveldb.DB;
 import rbq2012.leveldb.Iterator;
 
 import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public class ActivityMain extends Activity {
 
-	private File testFile;
+	private EditText etDir;
+	private TextView tvLog;
+	private StringBuilder sbLog;
 
 	public void btn0(View view){
 		DB db = null;
 		Iterator iter = null;
 		try {
-			db = new DB(testFile);
+			db = new DB(getDbDir());
 			db.open();
 			iter = db.iterator();
 			int count = 0;
 			for (iter.seekToFirst(); iter.isValid(); iter.next()) {
 				count++;
 			}
-			Toast.makeText(this, "len: " + count, Toast.LENGTH_SHORT).show();
+			log("found "+count+" items.");
 		}catch(Exception e) {
-			e.printStackTrace();
-			toast("failed.");
+			log("iterator failed.");
+			log(e);
 		}finally{
 			if(iter!=null) iter.close();
 			if(db!=null) db.close();
@@ -38,13 +44,13 @@ public class ActivityMain extends Activity {
 	public void btn1(View view) {
 		DB db=null;
 		try{
-			db= new DB(testFile);
+			db = new DB(getDbDir());
 			db.open();
-			db.put("Fuck who?".getBytes(),"Fuck me!".getBytes());
-			toast("done.");
+			db.put("testkey".getBytes(),"testval".getBytes());
+			log("db.put done.");
 		}catch(Exception e){
-			e.printStackTrace();
-			toast("failed.");
+			log("db.put failed.");
+			log(e);
 		}finally {
 			if(db!=null)db.close();
 		}
@@ -53,25 +59,47 @@ public class ActivityMain extends Activity {
 	public void btn2(View view) {
 		DB db=null;
 		try{
-			db= new DB(testFile);
+			db = new DB(getDbDir());
 			db.open();
-			toast(new String(db.get("Fuck who?".getBytes())));
+			log("db.get: "+new String(db.get("testkey".getBytes())));
 		}catch(Exception e){
-			e.printStackTrace();
-			toast("failed.");
+			log("db.get failed.");
+			log(e);
 		}finally {
 			if(db!=null)db.close();
 		}
 	}
 
-	private void toast(String text){
-		Toast.makeText(this,text,Toast.LENGTH_SHORT).show();
+	//Log text to a textview on the app.
+	private void log(String text){
+		sbLog.append(">>> ");
+		sbLog.append(text);
+		sbLog.append("\n");
+		tvLog.setText(sbLog.toString());
+	}
+
+	//Log exception to a textview on the app.
+	private void log(Exception e){
+		StringWriter sw=new StringWriter();
+		PrintWriter pw=new PrintWriter(sw);
+		e.printStackTrace(pw);
+		pw.flush();
+		log(sw.toString());
+		pw.close();
+	}
+
+	//Get the user spicified directory containing the leveldb database.
+	private File getDbDir(){
+		return new File(etDir.getText().toString());
 	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		testFile=new File("/sdcard/#aaa8/test");
+
+		etDir = (EditText) findViewById(R.id.editText);
+		tvLog = (TextView) findViewById(R.id.textView);
+		sbLog = new StringBuilder(">>> logcat\n");
 	}
 }
